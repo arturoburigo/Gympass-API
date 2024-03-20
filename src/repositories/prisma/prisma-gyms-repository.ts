18d3b2d8e684af7b938cc88cff18a1b72dfe8822
arturoburigo <burigoarturo3@gmail.com>
2024-03-20 00:12:13 -0300

    prisma repositories creations

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,40 @@
+import { Gym, Prisma } from "@prisma/client";
+import {
+  FindManyNearbyParams,
+  GymRepositoryInterface,
+} from "../gyms-repository";
+import { prisma } from "@/lib/prisma";
+
+export class PrismaGymsRepository implements GymRepositoryInterface {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: { id },
+    });
+    return gym;
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    //Shows only gyms on a 10km distance rate
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+      `;
+    return gyms;
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: { contains: query },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+    return gyms;
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({ data });
+    return gym;
+  }
+}
